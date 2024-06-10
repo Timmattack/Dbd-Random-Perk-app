adc14c140af204fe136cb4eee1433584ca3c8f08
Timothé <166731667+Timmattack@users.noreply.github.com>
2024-06-10 17:45:10 +0200

    On créé le dict dégeu des Surv/Killer X perks

--- sources/Datas.py
+++ sources/Datas.py
@@ -1,28 +1,62 @@
 import json
 
-def charge_file(cle, fichier: str = "cle_dict.json"):
-    f = open(fichier, "r")
-    tmp_cle: dict[chr, chr] = json.load(f)
-    for i in tmp_cle.keys():
-        cle[i] = tmp_cle[i]
+def charge_file(dict, fichier: str):
+    with open(fichier, "r") as f:
+        tmp_dict = json.load(f)
+    for k,v in tmp_dict.items():
+        dict[k] = v
+    
 
+def cool_print(dict):
+    for k in dict:
+        print(f"{k}: {dict[k]}")
 
-#print(attributs)
 
 '''
-IDsXNames = {}
+Characters
+"le num du surv": {"id": "", "name": "", "role": "", "image": ".png", "perks": ["Bond", "Leader", "Prove_Thyself"]}
 
-for i in range(len(Perks_IDs)):
-    IDsXNames[Perks_IDs[i]] = Perks_name[i]
-
-for k in IDsXNames.keys():
-    print("- "+k+": "+IDsXNames[k])
+Perks
+"le num de la perk": {"categories": [""], "name": "", "role": "", "character": int/null, "teachable": 40, "image": ".png"}
 '''
+
+def cross_Pid_Pname(dict1, dict2):
+    for k in dict1:
+        for i in range(3):
+            dict1[k]["perks"][i] = dict2[dict1[k]["perks"][i]]["name"]
+
 
 def main():
-    # un dict pour les perks
+    # un dict pour les Persos X leurs perks
+    Characters = {}
+    charge_file(Characters, "../data/Characters.json")
+    
+    """
+    On créé le dictionnaire des survivants avec leurs 3 compétences
+        Puis celui des tueurs
+    """
+    Survs_X_3Perks = { k:{"name": v["name"], "perks": v["perks"]} for k,v in Characters.items() if (v["role"] == "survivor")}
+    Killer_X_3Perks = { k:{"name": v["name"], "perks": v["perks"]} for k,v in Characters.items() if (v["role"] == "killer")}
+    
+    #cool_print(Survs_X_3Perks)
+    #cool_print(Killer_X_3Perks)
+    
+    
     Perks = {}
-    charge_file(Perks, "perks.json")
+    charge_file(Perks, "../data/Perks.json")
+    
+    Surv_null = { k:{"name": v["name"] } for k,v in Perks.items() if ((v["role"] == "survivor") and (v["character"] is None)) }
+    Killer_null = { k:{"name": v["name"] } for k,v in Perks.items() if ((v["role"] == "killer") and (v["character"] is None)) }
+    
+    
+    
+    cross_Pid_Pname(Survs_X_3Perks, Perks)
+    cross_Pid_Pname(Killer_X_3Perks, Perks)
+    
+    cool_print(Survs_X_3Perks)
+    cool_print(Killer_X_3Perks)
+    #cool_print(Surv_null)
+    #cool_print(Killer_null)
     
 
 if __name__ == "__main__":
