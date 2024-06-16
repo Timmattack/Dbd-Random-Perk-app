5177050a52f71e7f6f8cc98032a9bbba07e63f7b
Timothé <166731667+Timmattack@users.noreply.github.com>
2024-06-16 18:41:18 +0200

    Add files via upload

--- sources/app_setup.py
+++ sources/app_setup.py
@@ -3,28 +3,69 @@ from tkinter import messagebox
 
 from Datas import Init_Perks, cool_print_dict
 
-def show_checked(checkable_vars: dict):
-    checked_items = []
-    for k in checkable_vars:
-        if checkable_vars[k].get():
-            checked_items.append(f"{k}")
-
-    if checked_items:
-        messagebox.showinfo("Personnages possédés", "Vous avez: " + ", ".join(checked_items))
-    else:
-        messagebox.showinfo("Aucun personnage ?", "Vous n'avez personnes ?_?.")
+from random import randint
 
 
 def make_dict_checkable_vars(Role_Perks: dict):
-    checkable_vars = {k:0 for k in Role_Perks}
+    checkable_vars = {k:tk.BooleanVar() for k in Role_Perks}
     del checkable_vars["All"]
     
     return checkable_vars
 
+#On va chercher à générer la liste des perks possédées
+def get_checked(checkable_vars: dict):
+    checked_items = []
+    for k in checkable_vars:
+        if checkable_vars[k].get():
+            checked_items.append(f"{k}")
+    
+    return checked_items
+
+def build_checked_perks(checked_items, Role_Perks: dict):
+    checked_perks = [ perk
+        for k in checked_items
+        for perk in Role_Perks[k]
+    ]
+    
+    for perk in Role_Perks["All"]:
+        checked_perks.append(perk)
+    
+    return checked_perks
+
+#On choisit 4 compétences
+def place_i_end_list(i, L):
+    the_len = len(L)
+    if(i<the_len):
+        p = L[i]
+        L[i] = L[the_len-1]
+        L[the_len-1] = p
+
+
+def choose_4_perks(checked_perks: list[str]):
+    the_len = len(checked_perks)
+    for i in range(4):
+        p = randint(0,the_len-1-i)
+        place_i_end_list(p, checked_perks)
+    
+    return checked_perks[-4::]
+
+def show_random_perks(checkable_vars: dict, Role_perks):
+    checked_items = get_checked(checkable_vars)
+    
+    checked_perks = build_checked_perks(checked_items, Role_perks)
+    
+    the_perks = choose_4_perks(checked_perks)
+    
+    messagebox.showinfo("Vos compétences ce match", ", ".join(the_perks))
+    
 
 class Application(tk.Tk):
     def __init__(self, All_Perks):
         super().__init__()
+        
+        #stock All_perks
+        self.surv_perks = All_Perks["survivors"]
+        self.killer_perks = All_Perks["killers"]
         
         #Création de la fenêtre et sa taille
         self.title("Chaos mode, but the mode isn't live")
@@ -41,128 +82,75 @@ class Application(tk.Tk):
         y = (self.winfo_screenheight() // 2) - (height // 2)
         self.geometry('{}x{}+{}+{}'.format(width, height, x, y))
         
+        
+        self.show_main_page()
+
+    def show_main_page(self):
+        self.clear_content()
+        self.reset_grid_config()
+        
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=2)
+        self.grid_rowconfigure(2, weight=1)
+        
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=2)
+        self.grid_columnconfigure(2, weight=2)
+        self.grid_columnconfigure(3, weight=1)
+        
         #Première page de choix du rôle        
-        tk.Label(self, text="Choisissez votre rôle", font=("Arial", 24)).pack(pady=20)
+        tk.Label(self, text="Choisissez votre rôle", font=("Arial", 24), bg="#7d8cd2").grid(row=0, column=1, columnspan=2)
         
-        self.button1 = tk.Button(self, text="Survivant", command=lambda: self.survivor_window(All_Perks["survivors"]))
-        self.button1.pack(pady=10)
+        self.button1 = tk.Button(self, text="Survivant", font=("Arial", 24), command=lambda: self.role_window(self.surv_perks))
+        self.button1.grid(row=1, column=1, sticky='nsew', padx=5)
         
-        self.button2 = tk.Button(self, text="Tueur", command=lambda: self.killer_window(All_Perks["killers"]))
-        self.button2.pack(pady=10)
+        self.button2 = tk.Button(self, text="Tueur", font=("Arial", 24), command=lambda: self.role_window(self.killer_perks))
+        self.button2.grid(row=1, column=2, sticky='nsew', padx=5)
+
+    def role_window(self, role_perks):
+        self.clear_content()
+        self.reset_grid_config()
+        
+        checkable_vars = make_dict_checkable_vars(role_perks)
+        self.make_checkables_window(checkable_vars, role_perks)
     
+    def make_checkables_window(self, checkable_vars, role_perks):
+        the_len: int = len(checkable_vars)
+        
+        #Configuration de la grille
+        for i in range(the_len//4+1):
+            self.grid_rowconfigure(i, weight=1)
+        
+        self.grid_columnconfigure(0, weight=1)
+        for i in range(1,5):
+            self.grid_columnconfigure(i, weight=2)
+    
+        #Création des checkbox
+        checkboxes = []
+        for k in checkable_vars:
+            checkboxes.append(tk.Checkbutton(self, text=f"{k}", variable = checkable_vars[k], bg="#7d8cd2"))
+        
+        #Placement des checkbox
+        for i in range(the_len):
+            checkboxes[i].grid(row=i//4, column=i%4+1)
+    
+        btn_show = tk.Button(self, text="Alors, quelles compétences ?", command=lambda: show_random_perks(checkable_vars, role_perks), bg="#955555")
+        btn_back = tk.Button(self, text="Retour", command=self.show_main_page)
+        
+        btn_show.grid(row=the_len//4 + 1 )
+        btn_back.grid(row=0, column=0)
+
     def clear_content(self):
         for widget in self.winfo_children():
             widget.destroy()
     
-    def make_checkables_window(self, checkable_vars):
-        the_len: int = len(checkable_vars)
-        for i in range(the_len//4+1):
-            self.grid_rowconfigure(i, weight=1)
-        for i in range(4):
-            self.grid_columnconfigure(i, weight=1)
-    
-        #Création des variables pour checkbox
-        for k in checkable_vars:
-            checkable_vars[k]= tk.BooleanVar()
-    
-        #Création des boutons
-        checkboxes = []
-        for k in checkable_vars:
-            checkboxes.append(tk.Checkbutton(self, text=f"{k}", variable = checkable_vars[k], bg="#7d8cd2"))
+    def reset_grid_config(self):
+        # Réinitialiser les configurations de grid
+        for i in range(self.grid_size()[1]):  # Réinitialiser les lignes
+            self.grid_rowconfigure(i, weight=0)
+        for j in range(self.grid_size()[0]):  # Réinitialiser les colonnes
+            self.grid_columnconfigure(j, weight=0)
 
-        btn_show = tk.Button(self, text="Show Checked Options", command=lambda: show_checked(checkable_vars), bg="#955555")
-    
-        #Placement des boutons
-        for i in range(the_len):
-            checkboxes[i].grid(row=i//4, column=i%4)
-    
-        btn_show.grid(row=the_len//4 + 1 )
-    
-    def survivor_window(self, surv_perks):
-        self.clear_content()
-        
-        checkable_vars = make_dict_checkable_vars(surv_perks)
-        self.make_checkables_window(checkable_vars)
-    
-    def killer_window(self, killer_perks):
-        self.clear_content()
-        
-        checkable_vars = make_dict_checkable_vars(killer_perks)
-        self.make_checkables_window(checkable_vars)
-
-
-
-    
-"""
-def Launch_app(checkable_vars: dict, All_Perks: dict):
-    
-    Survs_dict = {k:0 for k in All_Perks["survivors"]}
-    del Survs_dict["All"]
-    
-    Killers_dict = {k:0 for k in All_Perks["killers"]}
-    del Killers_dict["All"]
-    
-    # Création de la fenêtre, et de sa taille
-    root = tk.Tk()
-    root.title("Chaos mode, but the mode isn't live")
-    root.iconbitmap('../tkinter/icone/skull.ico')
-    root.configure(background='#7d8cd2')
-
-    root.geometry("800x600")
-    root.resizable(False, False)
-
-    root.update_idletasks()
-    width = root.winfo_screenwidth() // 2 + root.winfo_screenwidth() // 4
-    height = root.winfo_screenheight() // 2 + root.winfo_screenheight() // 4
-    x = (root.winfo_screenwidth() // 2) - (width // 2)
-    y = (root.winfo_screenheight() // 2) - (height // 2)
-    root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
-    
-    
-    #Organisation de la taille des grilles
-    the_len: int = len(checkable_vars)
-    for i in range(the_len//4+1):
-        root.grid_rowconfigure(i, weight=1)
-    for i in range(4):
-        root.grid_columnconfigure(i, weight=1)
-    
-    #Création des variables pour checkbox
-    for k in checkable_vars:
-        checkable_vars[k]= tk.BooleanVar()
-    
-    #Création des boutons
-    checkboxes = []
-    for k in checkable_vars:
-        checkboxes.append(tk.Checkbutton(root, text=f"{k}", variable = checkable_vars[k], bg="#7d8cd2"))
-
-    btn_show = tk.Button(root, text="Show Checked Options", command=lambda: show_checked(checkable_vars), bg="#955555")
-    
-    #Placement des boutons
-    for i in range(the_len):
-        checkboxes[i].grid(row=i//4, column=i%4)
-    
-    btn_show.grid(row=the_len//4 + 1 )
-
-    # Start the main event loop
-    root.mainloop()
-"""
-
-#On va chercher à générer la liste des perks possédées
-def get_checked(checkable_vars: dict):
-    checked_items = []
-    for k in checkable_vars:
-        if checkable_vars[k].get():
-            checked_items.append(f"{k}")
-    
-    return checked_items
-
-def build_checked_perks(checked_items, Role_Perks):
-    checked_perks = [ perk
-        for k in checked_items
-        for perk in Role_Perks[k]
-    ]
-    
-    return checked_perks
     
 
 def main():
@@ -172,13 +160,13 @@ def main():
             "Dwight Fairfield": ['Bond', 'Leader', 'Prove Thyself'], 
             "Meg Thomas": ['Adrenaline', 'Quick & Quiet', 'Sprint Burst'], 
             "Claudette Morel": ['Botany Knowledge', 'Empathy', 'Self-Care'],
-            "All": ['Feur1', 'Feur2', 'Feur3', 'Feur4']
+            "All": ['sAll1', 'sAll2', 'sAll3', 'sAll4']
         }, 
         "killers": {
             "The Trapper": ['Agitation', 'Brutal Strength', 'Unnerving Presence'],
             "The Wraith": ['Bloodhound', 'Predator', 'Shadowborn'],
             "The Hillbilly": ['Enduring', 'Lightborn', 'Tinkerer'],
-            "All": ['Feur1', 'Feur2', 'Feur3', 'Feur4']
+            "All": ['kAll1', 'kAll2', 'kAll3', 'kAll4']
         }
     }
     
