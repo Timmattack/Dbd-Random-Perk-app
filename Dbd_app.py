45220bab8f210d2fd315286f174b9e38b7d2f7cd
Timothé <166731667+Timmattack@users.noreply.github.com>
2024-06-26 10:23:01 +0200

    Add files via upload

---
+++ Dbd_app.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+script_path = os.path.join(os.path.dirname(__file__), 'sources')
+sys.path.append(script_path)
+
+from APIs import update_all_data
+from app_setup import Application
+from Datas import Init_Perks, cool_print_dict
+
+
+def main():
+    
+    err: int = update_all_data("data/live_version.json", "data/local_version.json", "data/Perks.json", "data/Characters.json")
+    match err:
+        case 1:
+            print("Permission refusée: Le fichier \"local_Version.json\" ne peut être supprimé")
+        case 2:
+            print("Une erreur est survenue lors du remplacement de \"local_Version.json\"")
+        case 3:
+            print("Les compétences n'ont pas pu être récupérées")
+        case 4:
+            print("Les personnages n'ont pas pu être récupérés")
+        case _:
+            print("All Ok")
+        
+        
+    All_Perks = Init_Perks("data/Characters.json", "data/Perks.json")
+    
+    app = Application(All_Perks)
+    
+    app.mainloop()
+    
+
+if __name__ == "__main__":
+    main()
